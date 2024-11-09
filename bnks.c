9e2e2c5da8c015190bb485adbc32d7b016ddd648
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2024-11-09 12:10:00 +0530

    Update bnks.c

--- bnks.c
+++ bnks.c
@@ -1,62 +1,88 @@
 #include<stdio.h>
-#define MAX_P 5
-#define MAX_R 3
+#include<stdlib.h>
+#define P 5
+#define R 3
 
-int need[MAX_P][MAX_R];
-int avail[MAX_R];
-int allo[MAX_P][MAX_R];
-int finish[MAX_P];
+int need[P][R],avail[R],allo[P][R],finish[P];
+int count=0;
 
-int is_safe(){
-    int work[MAX_R];
-    for(int i = 0; i < MAX_R; i++)
-        work[i] = avail[i];
-
-    for(int i = 0; i < MAX_P; i++) {
-        if(finish[i] == 0) {
-            int j;
-            for(j = 0; j < MAX_R; j++) {
-                if(need[i][j] > work[j])
+int is_safe()
+{
+    for(int i=0;i<P;i++)
+    {
+        finish[i]=0;
+    }
+    int work[R];
+    for(int i=0;i<R;i++)
+    {
+        work[i]=avail[i];
+    }
+  for(int l=0;l<P;l++)
+    {
+        for(int i=0;i<P;i++)
+        {
+        if(finish[i]==0){
+            int flag=0;
+            for(int j=0;j<R;j++)
+            {
+                if(work[j]<need[i][j])
+                {
+                    flag=1;
                     break;
+                }
             }
-            if(j == MAX_R) {
-                for(int k = 0; k < MAX_R; k++)
-                    work[k] += allo[i][k];
-                finish[i] = 1;
-                i = -1; // Reset i to start from the beginning
+                if(flag==0)
+                {
+                    finish[i]=1;
+                    //count++;
+                    for(int k=0;k<R;k++)
+                    {
+                        work[k]+=allo[i][k];
+                    }
+                }
             }
         }
     }
-    for(int i = 0; i < MAX_P; i++) {
-        if(finish[i] == 0)
-            return 0;
+    for(int i=0;i<P;i++)
+    {
+        if(finish[i]==0)
+        {
+            return -1;
+        }
     }
     return 1;
 }
 
-int main() {
-    printf("Enter the allocation matrix :\n");
-    for(int i = 0; i < MAX_P; i++) {
-        for(int j = 0; j < MAX_R; j++) {
-            scanf("%d", &allo[i][j]);
-        }
-    }
-    printf("Enter the need matrix :\n");
-    for(int i = 0; i < MAX_P; i++) {
-        for(int j = 0; j < MAX_R; j++) {
-            scanf("%d", &need[i][j]);
-        }
-    }
-    printf("Enter the available matrix :\n");
-    for(int i = 0; i < MAX_R; i++) {
-        scanf("%d", &avail[i]);
-    }
 
-    if(is_safe() == 1) {
-        printf("System in safe state\n");
-    } else {
-        printf("The system is in an unsafe state\n");
+int main()
+{
+    printf("Allocation\n");
+    for(int i=0;i<P;i++)
+    {
+        for(int j=0;j<R;j++)
+        {
+            scanf("%d",&allo[i][j]);
+        }
     }
-    return 0;
+    printf("Need\n");
+    for(int i=0;i<P;i++)
+    {
+        for(int j=0;j<R;j++)
+        {
+            scanf("%d",&need[i][j]);
+        }
+    }
+    printf("Available\n");
+    for(int i=0;i<R;i++)
+    {
+        scanf("%d",&avail[i]);
+    }
+    if(is_safe()==1)
+    {
+        printf("SAFE\n");
+    }
+    else
+    {
+        printf("UNSAFE\n");
+    }
 }
-
